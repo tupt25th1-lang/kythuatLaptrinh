70061700a5e293ac5285095a33af7bb9fc219c67
buoi1 <PMT201@PMT3016>
2026-03-06 10:55:50 +0700

    lamxongbuoi1

--- KYTHUATLAPTRINH/BUOI1/BUOI1.cpp
+++ KYTHUATLAPTRINH/BUOI1/BUOI1.cpp
@@ -1,1 +1,65 @@
-utuyty
+#include < iostream>
+#include < vector>
+using namespace std;
+struct  sinhvien {
+	string hoten;
+	int tuoi;
+	bool gioitinh;
+	string diachi;
+
+
+};
+int main() {
+	sinhvien b;
+	b.hoten = " Nguyen van A ";
+	b.tuoi = 20;
+	b.gioitinh = true;
+	b.diachi = " Tay Ninh ";
+	
+	sinhvien c;
+	c.hoten = " Tran Thi B";
+	c.tuoi = 30;
+	 
+	vector <sinhvien> mang;
+	mang.push_back(b);
+	mang.push_back(c);
+	for (int i = 0; i < mang.size(); i++) {
+		cout << " sinh vien" << (i + 1) << endl;
+		cout << " \ t + Ho ten : " << mang[i].hoten << endl;
+		cout << " \ t+ Tuoi : " << mang[i].tuoi << endl;
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//	int a = 772;
+//	int* p = &a;
+//	a += 2929;
+//	a = *p + 2;
+//	cout << *p;
+//}
+//*
+//	vector < float > diemthi;
+//	diemthi.push_back(5.5);
+//	diemthi.push_back(4);
+//	diemthi.push_back(6);
+//	diemthi.pop_back();
+//	for (int i = 0; i < diemthi.size(); i++) {
+//		cout << diemthi[i] << endl;
+//	
+//
+//
+//
